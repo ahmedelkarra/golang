2680b258bfa440a63c0218b692684479d76b2d63
unknown <ahmedelkarra@gmail.com>
2026-01-17 13:25:42 +0100

    first commit

--- auth/jwtGen.go
+++ auth/jwtGen.go
@@ -1,21 +1,13 @@
 package auth
 
 import (
-	"log"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/lpernett/godotenv"
 )
 
 func JwtGen(id string) (string, error) {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Println("Error loading .env file")
-	}
-
 	secretKey := []byte(os.Getenv("secretKey"))
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
