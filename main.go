679f322e781af7261b509451cd13af6a7a04fb13
unknown <ahmedelkarra@gmail.com>
2026-01-17 12:53:38 +0100

    first commit

--- main.go
+++ main.go
@@ -15,15 +15,15 @@ func main() {
 	err := godotenv.Load()
 
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	if err := database.Connect(); err != nil {
-		log.Fatal("Failed to connect to database:", err)
+		log.Println("Failed to connect to database:", err)
 	}
 
 	if err := database.DB.AutoMigrate(&models.User{}); err != nil {
-		log.Fatal(err)
+		log.Println(err)
 	}
 
 	r := gin.Default()
